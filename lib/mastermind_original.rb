b0f27b8058052d2252af77b6e992073c6cce2089
Javi Filella <txusko@gmail.com>
2018-02-26 18:28:11 +0100

    Additional commands & improvements

--- lib/mastermind_original.rb
+++ lib/mastermind_original.rb
@@ -1,18 +1,18 @@
 # Constacts
 ITEMS = '1 2 3 4 5 6 7 8'.split
-MAX_ATTEMPS = 10
+MAX_ATTEMPTS = 10
 COMBINATION = 4
 
 # MasterMind class
 class MasterMind
-  attr_accessor :win_combination, :attemps
+  attr_accessor :win_combination, :attempts
 
   def initialize
     init
   end
 
   def init
-    @attemps = 0
+    @attempts = 0
     @win_combination = []
     (0...COMBINATION).each do |i|
       @win_combination[i] = ITEMS.sample(1).first
@@ -28,18 +28,18 @@ class MasterMind
     system 'clear'
     puts ' WELCOME TO MASTERMIND!!!!'
     draw_line
-    puts " You have #{MAX_ATTEMPS} attemps to find out the correct combination."
+    puts " You have #{MAX_ATTEMPTS} attempts to find out the correct combination."
     puts " The combination is a random string composed by #{COMBINATION} " \
          'sepparated comma items.'
     print ' You can combine the following items one or more times:'
     puts " #{show_combination(ITEMS)}"
-    puts " Example: #{show_combination(ITEMS.sample(COMBINATION))}"
-    puts ' Game commands: '
-    puts ' - new/newgame(finish current game and start a new one)'
-    puts ' - endgame/end (end current game)'
-    puts ' - attemp/attemps (show number of attemps)'
-    puts ' - godmode (show winner combination)'
-    puts ' - quit/exit (exit game)'
+    puts " Combination example: #{show_combination(ITEMS.sample(COMBINATION))}"
+    puts ' Additional game commands: '
+    puts ' - new/newgame/n (finish current game and start a new one)'
+    puts ' - attempt/attempts/a (show number of attempts)'
+    puts ' - withdraw/w (give up game)'
+    puts ' - godmode/! (show winner combination)'
+    puts ' - quit/exit/q (exit game)'
     draw_line
     puts ' Enter a combination:'
   end
@@ -58,24 +58,34 @@ class MasterMind
   def check_command?(option)
     return false unless option
     case option.first
-    when 'new', 'newgame'
+    when 'new', 'newgame', 'n'
       option.clear
       init
-    when 'godmode'
+    when 'attempts', 'attempt', 'a'
+      option.clear
+      puts " ** Attempt number #{@attempts + 1} of #{MAX_ATTEMPTS} ** "
+      draw_line
+    when 'withdraw', 'w'
+      option.clear
+      lose
+      return true unless playagain?
+    when 'godmode', '!'
       option.clear
       puts " ** Winner combination: #{show_combination(@win_combination)} ** "
-    when 'attemps', 'attemp'
-      option.clear
-      puts " ** Attemp number #{@attemps + 1} of #{MAX_ATTEMPS} ** "
-    when 'end', 'endgame'
-      puts ' ** Game finished. ** '
-      return true unless playagain?
-      init
-      option.clear
-    when 'quit', 'exit'
+      draw_line
+    when 'quit', 'exit', 'q'
       return true
     end
     false
+  end
+
+  def lose
+    puts '  YOU LOSE!!'
+    puts "  The correct answer was: #{show_combination(@win_combination)}."
+  end
+
+  def win
+    puts '  YOU WIN!!!'
   end
 
   def check_items?(combination)
@@ -91,6 +101,7 @@ class MasterMind
     return true if hits == @win_combination.length
     partial_hits = partial_hits(combination_cp, win_combination_cp)
     puts " You have #{hits} correct hits and #{partial_hits} partial hits"
+    draw_line
     false
   end
 
@@ -118,6 +129,7 @@ class MasterMind
 
   def play
     while (combination = read_option)
+      draw_line
       # Custom commands
       return false if check_command?(combination)
       next if combination.empty?
@@ -131,42 +143,39 @@ class MasterMind
   end
 
   def play_combination?(combination)
-    return false unless evaluate?(combination)
-    return false unless check_attemps?(combination)
-    return false unless check_combination?(combination)
-    puts '  YOU WIN!!'
-    true
-  end
-
-  def check_attemps?(combination)
-    if @attemps >= MAX_ATTEMPS
-      # Last combination atemp
-      puts '  YOU WIN!!!' if check_combination?(combination)
-      # User have lose the game
-      puts "  The correct answer was: #{show_combination(@win_combination)}."
-      puts '  YOU LOSE!!'
-      return false
+    if @attempts >= MAX_ATTEMPTS
+      # Check last combination
+      check_combination?(combination) ? win : lose
+    else
+      return false unless evaluate?(combination)
+      return false unless check_combination?(combination)
+      win
     end
+    #playagain?
     true
   end
 
   def evaluate?(combination)
-    @attemps += 1
-    puts " ** Attemps number #{@attemps} of #{MAX_ATTEMPS} ** "
+    @attempts += 1
+    puts " ** Attempt number #{@attempts} of #{MAX_ATTEMPTS} ** "
     if combination.length != COMBINATION
-      puts " Invalid attemp. Ex: #{show_combination(ITEMS.sample(COMBINATION))}."
+      puts " Invalid attempt. Ex: #{show_combination(ITEMS.sample(COMBINATION))}."
       puts ' Try again!'
     else
       return true if check_items?(combination)
       puts " Invalid items! Possible items: #{show_combination(ITEMS)}"
     end
+    draw_line
     false
   end
 
   def playagain?
+    draw_line
     puts 'Do you want to play again? (y/n)'
     answer = gets.chomp
-    (answer == 'y')
+    return false if answer != 'y'
+    init
+    true
   end
 end
 
